5c55157c3b4817ea1698b6b36802f96df33ed35c
Oronoque <110566130+Oronoque@users.noreply.github.com>
2023-02-07 13:04:17 -0600

    Test commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Bootstrap</h2>
+        <h3>Holy shit it works!</h3>
+      </div>
+    );
+  }
+}
